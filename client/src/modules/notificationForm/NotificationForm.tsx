500aa03ba8fd80f3dacf9b80b78d2cfd74ee95e2
Kamil Grabek <kamil.grabek@allegro.pl>
2019-11-20 19:51:24 +0100

    wydzielenie komponentów formularza input i checkbox

--- client/src/modules/notificationForm/NotificationForm.tsx
+++ client/src/modules/notificationForm/NotificationForm.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Checkbox, Dropdown, Form, Icon, Input, Menu, Modal, Tooltip } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
+import { GetFieldDecoratorOptions, WrappedFormUtils } from 'antd/lib/form/Form';
 import React, { FC, useEffect } from 'react';
 import deleteSingleSubscription from '../../deleteSingleSubscription';
 
@@ -14,6 +15,8 @@ function hasErrors(fieldsError: any) {
 }
 
 let id = 0;
+
+const UrlRegex = new RegExp('(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?');
 
 export interface NotificationFormProps {
   copiedNotification: any;
@@ -37,15 +40,58 @@ interface FormValues {
   names: any[];
 }
 
-const { confirm } = Modal;
+interface FormCheckboxProps {
+  fieldId: string;
+  label: string;
+  hint: string;
+  formUtils: WrappedFormUtils<FormValues>;
+  options?: GetFieldDecoratorOptions;
+}
+
+interface FormInputProps {
+  fieldId: string;
+  label: string;
+  hint: string;
+  iconType: string;
+  placeholder: string;
+  formUtils: WrappedFormUtils<FormValues>;
+  options?: GetFieldDecoratorOptions;
+}
+
+const FormInput = ({ formUtils, label, fieldId, placeholder, options, hint, iconType }: FormInputProps) => {
+  const { isFieldTouched, getFieldError, getFieldDecorator } = formUtils;
+  const error = isFieldTouched(fieldId) && getFieldError(fieldId);
+
+  return (
+    <Form.Item label={label} validateStatus={error ? 'error' : ''} help={error || ''}>
+      <Tooltip title={hint}>
+        {getFieldDecorator(fieldId, options)(
+          <Input
+            prefix={<Icon type={iconType} style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder={placeholder}
+          />,
+        )}
+      </Tooltip>
+    </Form.Item>
+  );
+};
+
+const FormCheckbox = ({ formUtils: { getFieldDecorator }, fieldId, label, hint, options }: FormCheckboxProps) => (
+  <Form.Item label={label}>
+    <Tooltip title={hint}>
+      {getFieldDecorator(fieldId, options)(
+        <Checkbox />,
+      )}
+    </Tooltip>
+  </Form.Item>
+);
+
 const NotificationForm: FC<FormComponentProps<FormValues> & NotificationFormProps> = (props) => {
   const {
     getFieldDecorator,
     getFieldsError,
-    getFieldError,
     getFieldValue,
     getFieldsValue,
-    isFieldTouched,
     setFields,
   } = props.form;
 
@@ -55,80 +101,24 @@ const NotificationForm: FC<FormComponentProps<FormValues> & NotificationFormProp
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleSubmit = (event: any) => {
+  const sendNotification = (event: any) => {
     event.preventDefault();
     props.form.validateFields((err, values) => {
       if (!err) {
         const { keys, names, ...rest } = values;
 
-        props.onSend({ actions: names ? names.filter(Boolean).map((name, index) => ({ action: index, title: name })) : [], ...rest });
+        props.onSend({
+          actions: names ? names.filter(Boolean).map((name, index) => ({ action: index, title: name })) : [],
+          ...rest,
+        });
       }
     });
   };
-  // Only show error after a field is touched.
-  const titleError = isFieldTouched('title') && getFieldError('title');
-  const bodyError = isFieldTouched('body') && getFieldError('body');
-  const iconError = isFieldTouched('icon') && getFieldError('icon');
-  const badgeError = isFieldTouched('badge') && getFieldError('badge');
-  const imageError = isFieldTouched('image') && getFieldError('image');
-  const remove = (k: number) => {
-    const { form } = props;
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys');
-
-    // can use data-binding to set
-    form.setFieldsValue({
-      keys: keys.filter((key: number) => key !== k),
-    });
-  };
-  const add = () => {
-    const { form } = props;
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys');
-    const nextKeys = keys.concat(id++);
-
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      keys: nextKeys,
-    });
-  };
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 8 },
-      sm: { span: 6 },
-      md: { span: 10 },
-    },
-    wrapperCol: {
-      xs: { span: 16 },
-      sm: { span: 18 },
-      md: { span: 14 },
-    },
-  };
-
-  getFieldDecorator('keys', { initialValue: [] });
-
-  const keys = getFieldValue('keys');
-  const formItems = keys.map((k: any, index: number) => (
-    <Form.Item
-      label={['First action', 'Second action', 'Third action'][index]}
-      required={false}
-      key={k}
-    >
-      {getFieldDecorator(`names[${k}]`)(<Input placeholder="Enter action title" style={{ width: '80%', marginRight: 8 }} />)}
-      <Icon
-        className="dynamic-delete-button"
-        type="minus-circle-o"
-        onClick={() => remove(k)}
-      />
-    </Form.Item>
-  ));
   const deleteSubscription = async () => {
     await deleteSingleSubscription(props.userSubscription.subscription.endpoint);
   };
-
-  function showDeleteConfirm() {
-    confirm({
+  const showDeleteConfirm = () => {
+    Modal.confirm({
       title: 'Are you sure delete this subscription?',
       content: 'You can add subscription again later.',
       okText: 'Yes',
@@ -144,7 +134,45 @@ const NotificationForm: FC<FormComponentProps<FormValues> & NotificationFormProp
         console.log('Cancel');
       },
     });
-  }
+  };
+  const removeAction = (actionKey: string) => {
+    const { form } = props;
+    const keys = form.getFieldValue('keys');
+
+    form.setFieldsValue({
+      keys: keys.filter((key: string) => key !== actionKey),
+    });
+  };
+  const addAction = () => {
+    const { form } = props;
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(id++);
+
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  };
+
+  getFieldDecorator('keys', { initialValue: [] });
+  const keys = getFieldValue('keys');
+  const actionsItems = keys.map((key: string, index: number) => (
+    <Form.Item
+      label={['First action', 'Second action', 'Third action'][index]}
+      key={key}
+    >
+      {getFieldDecorator(`names[${key}]`)(
+        <Input
+          placeholder="Enter action title"
+          style={{ width: '80%', marginRight: 8 }}
+        />
+      )}
+      <Icon
+        className="dynamic-delete-button"
+        type="minus-circle-o"
+        onClick={() => removeAction(key)}
+      />
+    </Form.Item>
+  ));
 
   const menu = (
     <Menu>
@@ -173,12 +201,23 @@ const NotificationForm: FC<FormComponentProps<FormValues> & NotificationFormProp
   const type = oses[props.userSubscription.info.os];
 
   return (
-    <Form {...formItemLayout} onSubmit={handleSubmit}>
+    <Form
+      labelCol={{
+        xs: { span: 8 },
+        sm: { span: 6 },
+        md: { span: 10 },
+      }}
+      wrapperCol={{
+        xs: { span: 16 },
+        sm: { span: 18 },
+        md: { span: 14 },
+      }}
+      onSubmit={sendNotification}
+    >
       <Card
         key={props.userSubscription.subscription.endpoint}
         title={
           <>
-
             {type ? (
             <Icon type={type} style={{ fontSize: 32 }} />
           ) : props.userSubscription.info.os}
@@ -201,97 +240,102 @@ const NotificationForm: FC<FormComponentProps<FormValues> & NotificationFormProp
             </div>
         }
       >
-        <Form.Item validateStatus={titleError ? 'error' : ''} help={titleError || ''} label="Title">
-          <Tooltip title="Title of notification will appear at the top of Your notification.">
-            {getFieldDecorator('title', {
-              initialValue: '',
-              rules: [{ required: true, message: 'Please input notification title.' }],
-            })(
-              <Input
-                placeholder="Enter title"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item validateStatus={bodyError ? 'error' : ''} help={bodyError || ''} label="Body">
-          <Tooltip title="Notification body text. It can be long, multiline text.">
-            {getFieldDecorator('body', {
-              initialValue: '',
-              rules: [{ required: true, message: 'Please input notification body.' }],
-            })(
-              <Input
-                placeholder="Enter body"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Icon" validateStatus={iconError ? 'error' : ''} help={iconError || ''}>
-          <Tooltip title="Icon displayed in notification.">
-            {getFieldDecorator('icon', {
-              initialValue: '',
-              rules: [{ pattern: new RegExp('(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?'), message: 'Please input valid url.' }],
-            })(
-              <Input
-                prefix={<Icon type="question" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Enter icon url"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Badge" validateStatus={badgeError ? 'error' : ''} help={badgeError || ''}>
-          <Tooltip title="Badge is icon displayed in mobile devices toolbar.">
-            {getFieldDecorator('badge', {
-              initialValue: '',
-              rules: [{ pattern: new RegExp('(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?'), message: 'Please input valid url.' }],
-            })(
-              <Input
-                prefix={<Icon type="trademark" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Enter badge url"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Image" validateStatus={imageError ? 'error' : ''} help={imageError || ''}>
-          <Tooltip title="Image displayed after notification body.">
-            {getFieldDecorator('image', {
-              initialValue: '',
-              rules: [{ pattern: new RegExp('(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?'), message: 'Please input valid url.' }],
-            })(
-              <Input
-                prefix={<Icon type="trademark" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Enter image ulr"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Vibrate">
-          <Tooltip title="Vibration pattern for the device's vibration hardware to emit when the notification fires.">
-            {getFieldDecorator('vibrate', { initialValue: '' })(
-              <Input
-                prefix={<Icon type="trademark" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Enter vibration pattern"
-              />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Renotify">
-          <Tooltip title="Specifies whether the user should be notified after a new notification replaces an old one.">
-            {getFieldDecorator('renotify', { initialValue: false })(
-              <Checkbox />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        <Form.Item label="Require action">
-          <Tooltip title="Active until the user clicks or dismisses it.">
-            {getFieldDecorator('requireInteraction', { initialValue: false })(
-              <Checkbox />,
-            )}
-          </Tooltip>
-        </Form.Item>
-        {formItems}
+        <FormInput
+          fieldId="title"
+          label="Title"
+          placeholder="Enter title"
+          hint="Title of notification will appear at the top of Your notification."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{
+            initialValue: '',
+            rules: [{
+              required: true,
+              message: 'Please input notification title.',
+            }],
+          }}
+        />
+        <FormInput
+          fieldId="body"
+          label="Body"
+          placeholder="Enter body"
+          hint="Notification body text. It can be long, multiline text."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{ initialValue: '' }}
+        />
+        <FormInput
+          fieldId="icon"
+          label="Icon"
+          placeholder="Enter icon url"
+          hint="Icon displayed in notification."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{
+            initialValue: '',
+            rules: [{
+              pattern: UrlRegex,
+              message: 'Please input valid url.',
+            }],
+          }}
+        />
+        <FormInput
+          fieldId="badge"
+          label="Badge"
+          placeholder="Enter badge url"
+          hint="Badge is icon displayed in mobile devices toolbar."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{
+            initialValue: '',
+            rules: [{
+              pattern: UrlRegex,
+              message: 'Please input valid url.',
+            }],
+          }}
+        />
+        <FormInput
+          fieldId="image"
+          label="Image"
+          placeholder="Enter image url"
+          hint="Image displayed after notification body."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{
+            initialValue: '',
+            rules: [{
+              pattern: UrlRegex,
+              message: 'Please input valid url.',
+            }],
+          }}
+        />
+        <FormInput
+          fieldId="vibrate"
+          label="Vibrate"
+          placeholder="Enter image url"
+          hint="Vibration pattern for the device's vibration hardware to emit when the notification fires."
+          iconType="trademark"
+          formUtils={props.form}
+          options={{ initialValue: '' }}
+        />
+        <FormCheckbox
+          fieldId="renotify"
+          label="Renotify"
+          hint="Specifies whether the user should be notified after a new notification replaces an old one."
+          formUtils={props.form}
+          options={{ initialValue: false }}
+        />
+        <FormCheckbox
+          fieldId="requireInteraction"
+          label="Require action"
+          hint="Active until the user clicks or dismisses it."
+          formUtils={props.form}
+          options={{ initialValue: false }}
+        />
+        {actionsItems}
         {keys.length < 3 && (
           <Form.Item label={['First action', 'Second action', 'Third action'][keys.length]}>
-            <Button type="dashed" onClick={add} style={{ width: '80%' }}>
+            <Button type="dashed" onClick={addAction} style={{ width: '80%' }}>
               <Icon type="plus" /> Add action
             </Button>
           </Form.Item>
